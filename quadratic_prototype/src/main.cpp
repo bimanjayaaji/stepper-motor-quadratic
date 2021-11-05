5755b4f3bf1a5e33f37c2f2eb77fcc0799af3e6d
bimanjayaaji <anjay4705@gmail.com>
2021-11-05 16:54:32 +0700

    minor changes

--- quadratic_prototype/src/main.cpp
+++ quadratic_prototype/src/main.cpp
@@ -1,9 +1,42 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+//defines pins
+const int stepPin = 3;  //PUL -Pulse
+const int dirPin = 5; //DIR -Direction
+const int enPin = 6;  //ENA -Enable
+
+void setup(){
+  //Sets the pins as Outputs
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+  pinMode(enPin,OUTPUT);
+  digitalWrite(enPin,LOW);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  //Enables the motor direction to move
+  digitalWrite(dirPin,HIGH);
+  //Makes 200 Pulses for making one full cycle rotation
+  for(int x = 0; x < 1600; x++){
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(500); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(500); 
+  }
+  
+  //One second delay
+  delay(1000);
+
+  //Changes the rotations direction
+  digitalWrite(dirPin,LOW);
+  // Makes 200 pulses for making one full cycle rotation
+  for(int x = 0; x < 1600; x++) {
+    digitalWrite(stepPin,HIGH);
+    delayMicroseconds(500);
+    digitalWrite(stepPin,LOW);
+    delayMicroseconds(500);
+  }
+  
+  //One second delay
+  delay(1000); 
 }
