adb65f54644c8958da99b417dfcc9f375c7bb538
bimanjayaaji <anjay4705@gmail.com>
2021-11-06 11:05:02 +0700

    add new script

--- quadratic_prototype/src/main.cpp
+++ quadratic_prototype/src/main.cpp
@@ -1,42 +1,83 @@
+/*
+
+This script contains algorithm for :
+  - guadratic movement with direct delay based speed control
+    (move60quad)
+
+*/
+
 #include <Arduino.h>
 
-//defines pins
-const int stepPin = 3;  //PUL -Pulse
-const int dirPin = 5; //DIR -Direction
-const int enPin = 6;  //ENA -Enable
+#define pul 3
+#define dir 5
+#define en 6
+#define pulse_per_rot 1600
+
+bool en_state=0;
+
+float degrees = 60; 
+float angres = (degrees/360)*pulse_per_rot;
+float A = 2 ,B = 0.25;
+float E1 = -1.5, E2 = 1.5, F = (E2-E1)/angres;
+float multiplier = 500;
+
+/*
+
+                Quadratic Graph
+_____________________________________________
+| num | A   | B   | E1  | E2  |   F   | T(s)|
+| 1   | 2   | 0.25| -1.5| 1.5 |0.01123| 0.59|
+| 2   | 
+|_____|_____|_____|_____|_____|_______|_____|
+
+Notations:
+A         = Constant of x^2
+B         = Constant "c"
+E1 and E2 = range of the desired curve area
+F         = increment of the range associated with angres
+T(s)      = time needed each cycle
+
+*/
+
+void motor_setup(){
+  pinMode(pul, OUTPUT); 
+  pinMode(dir, OUTPUT); 
+  pinMode(en, OUTPUT);
+  digitalWrite(en, en_state);
+}
+
+float getDelayquad(float x){
+  float y = (A*x*x) + B;
+  Serial.println(y);
+  return y*multiplier;
+}
+
+void move60quad(bool var){
+  float k = 0, l = E1;
+  double start = millis();
+  digitalWrite(dir,var);
+  while (k < angres){
+    if (l < E2){
+      float ddelay = getDelayquad(l);
+      digitalWrite(pul,HIGH);
+      delayMicroseconds(ddelay);
+      digitalWrite(pul,LOW);
+      delayMicroseconds(ddelay);
+      l += F;
+    }
+    k += 1;
+  }
+  Serial.println((millis()-start)/1000);
+}
 
 void setup(){
-  //Sets the pins as Outputs
-  pinMode(stepPin,OUTPUT); 
-  pinMode(dirPin,OUTPUT);
-  pinMode(enPin,OUTPUT);
-  digitalWrite(enPin,LOW);
+  Serial.begin(115200);
+  motor_setup();
 }
 
 void loop(){
-  //Enables the motor direction to move
-  digitalWrite(dirPin,HIGH);
-  //Makes 200 Pulses for making one full cycle rotation
-  for(int x = 0; x < 1600; x++){
-    digitalWrite(stepPin,HIGH); 
-    delayMicroseconds(500); 
-    digitalWrite(stepPin,LOW); 
-    delayMicroseconds(500); 
-  }
-  
-  //One second delay
+  move60quad(HIGH);
   delay(1000);
-
-  //Changes the rotations direction
-  digitalWrite(dirPin,LOW);
-  // Makes 200 pulses for making one full cycle rotation
-  for(int x = 0; x < 1600; x++) {
-    digitalWrite(stepPin,HIGH);
-    delayMicroseconds(500);
-    digitalWrite(stepPin,LOW);
-    delayMicroseconds(500);
-  }
-  
-  //One second delay
-  delay(1000); 
+  move60quad(LOW);
+  delay(1000);
 }
