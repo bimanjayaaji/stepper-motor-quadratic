e18ccbaf3a447a60b649d9340e93473d453c74b6
bimanjayaaji <anjay4705@gmail.com>
2021-11-11 11:25:32 +0700

    minor management and add calibrate lim_sw func

--- quadratic_prototype/src/main.cpp
+++ quadratic_prototype/src/main.cpp
@@ -11,14 +11,15 @@ This script contains algorithm for :
 #define pul 3
 #define dir 5
 #define en 6
+#define lim 7
 #define pulse_per_rot 1600
 
 bool en_state=0;
 
 float degrees = 60; 
 float angres = (degrees/360)*pulse_per_rot;
-float A = 2 ,B = 0.25;
-float E1 = -1.5, E2 = 1.5, F = (E2-E1)/angres;
+float A = 3 ,B = 0.18;
+float E1 = -1.1, E2 = 1.1, F = (E2-E1)/angres;
 float multiplier = 500;
 
 /*
@@ -27,7 +28,9 @@ float multiplier = 500;
 _____________________________________________
 | num | A   | B   | E1  | E2  |   F   | T(s)|
 | 1   | 2   | 0.25| -1.5| 1.5 |0.01123| 0.59|
-| 2   | 
+| 2   | 2   | 0.23| -1.4| 1.4 |0.01048| 0.53|
+| 3   | 2   | 0.20| -1.3| 1.3 |0.00973| 0.47|
+| 4   | 3   | 0.18| -1.1| 1.1 |0.00823| 0.49|
 |_____|_____|_____|_____|_____|_______|_____|
 
 Notations:
@@ -68,6 +71,18 @@ void move60quad(bool var){
     k += 1;
   }
   Serial.println((millis()-start)/1000);
+}
+
+void calibrate(){
+  bool lim_sw = false;
+  while(lim_sw){
+    digitalWrite(dir,HIGH);
+    digitalWrite(pul,HIGH);
+    delayMicroseconds(5000);
+    digitalWrite(pul,LOW);
+    delayMicroseconds(5000);
+    lim_sw = digitalRead(lim);
+  }  
 }
 
 void setup(){
